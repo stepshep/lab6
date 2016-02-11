4c6cf001f6cad5da6e41972fff230052e6a8dde1
Stephen Shepherd <slshephe@ucsd.edu>
2016-02-10 21:51:00 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,10 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	
+    $.get("/project/" + idNumber, myFunc);
+		
+	console.log("/project/" + idNumber);
 }
 
 /*
@@ -35,4 +39,25 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", paletteFunc);
+}
+
+
+function myFunc(e) {
+	console.log(e);
+	
+	var projectHTML = '<img src="' + e['image'] + '" class="detailsImage">' +
+	'<p><h1>' + e['title'] + ' - ' + e['date'] + '</h1></p>' +
+	'<p>' + e['summary'] + '</p>';
+	
+	$('#project' + e['id'] + ' ' + '.details').html(projectHTML);
+}
+
+function paletteFunc(e) {
+var colors = e['colors']['hex'];
+$('body').css('background-color', colors[0]);
+$('.thumbnail').css('background-color', colors[1]);
+$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+$('p').css('color', colors[3]);
+$('.project img').css('opacity', .75);
 }
